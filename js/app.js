40939a01f967f48e9714cbeb74afda6d21523595
Fábio Pimentel <fabio.pimentel@taointerativa.com.br>
2020-01-09 12:14:50 -0300

    Atualizações 2019

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
+var apresentacao = Vue.component('apresentacao',{
+    template: '#apresentacao'
+});
+var skills = Vue.component('skills',{
+    template: '#skills'
+});
+var exp = Vue.component('exp',{
+    template: '#exp'
+});
+var jobs = Vue.component('jobs',{
+    template: '#jobs'
+});
+
+var router = new VueRouter({
+    routes: [
+        {path: '/', component: apresentacao},
+        {path: '/habilidades', component: skills},
+        {path: '/experiencia-profissional', component: exp},
+        {path: '/trabalhos-feitos', component: jobs}
+    ]
+})
+
 var app = new Vue({
     el: '#app',
+    router,
     data:{
 
     },
